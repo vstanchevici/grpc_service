d2d22ac057cb533f037993d60644c473c72eda7a
Victor Stanchevici <victor@xuver-services.com>
2021-02-01 23:50:08 +0200

    Add grpc

---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <version.h>
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+    std::cout << "!Project Name: " << GRPC_SERVICE_PROJECT_DESCRIPTION << std::endl;
+    return 0;
+}
