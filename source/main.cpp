6c5266540a613aebc0368e67a7ba17784ee856bf
Victor Stanchevici <vstanchevici@yahoo.com>
2023-01-28 01:10:33 +0200

    Update/fix

--- source/main.cpp
+++ source/main.cpp
@@ -3,7 +3,7 @@
 #include <filesystem>
 #include <vector>
 
-#include <s3/httpexample.grpc.pb.h>
+#include <grpc/s3/httpexample.grpc.pb.h>
 
 #include <grpcpp/ext/proto_server_reflection_plugin.h>
 #include <grpcpp/grpcpp.h>
@@ -30,18 +30,22 @@ class HTTPExampleServiceImpl final : public HTTPExample::Service
     }
 };
 
-void RunServer() {
+void RunServer()
+{
     std::string server_address("0.0.0.0:8880");
     HTTPExampleServiceImpl service;
 
     //grpc::EnableDefaultHealthCheckService(true);
     //grpc::reflection::InitProtoReflectionServerBuilderPlugin();
     ServerBuilder builder;
+
     // Listen on the given address without any authentication mechanism.
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
     // Register "service" as the instance through which we'll communicate with
     // clients. In this case it corresponds to an *synchronous* service.
     builder.RegisterService(&service);
+
     // Finally assemble the server.
     std::unique_ptr<Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << server_address << std::endl;
